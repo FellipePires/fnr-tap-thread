ff43ad4e6ed44cdba40c22855cf6d83912049dda
Rodrigopds <rodrigonovaroma@hotmail.com>
2018-04-13 22:18:05 -0300

    Funcionario adicionado

--- src/br/com/fnr_tap/entidades/Cliente.java
+++ src/br/com/fnr_tap/entidades/Cliente.java
@@ -7,17 +7,41 @@ public class Cliente {
 	private ArrayList<Produto> produtos;
 	private String nome;
 	private boolean isPreferencial;
-	
-	public int getCodigo() {	return codigo;	}
-	public void setCodigo(int codigo) {		this.codigo = codigo;		}
-	public ArrayList<Produto> getProdutos() {		return produtos;		}
-	public void setProdutos(ArrayList<Produto> produtos) {	this.produtos = produtos;	}
-	public String getNome() {	return nome;	}
-	public void setNome(String nome) {	this.nome = nome;		}
-	public boolean isPreferencial() {	return isPreferencial;		}
-	public void setPreferencial(boolean isPreferencial) {	this.isPreferencial = isPreferencial;	}
 
-	public Cliente() {	}
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public ArrayList<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(ArrayList<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public boolean isPreferencial() {
+		return isPreferencial;
+	}
+
+	public void setPreferencial(boolean isPreferencial) {
+		this.isPreferencial = isPreferencial;
+	}
+
+	public Cliente() {
+	}
 
 	public Cliente(int codigo, ArrayList<Produto> produtos, String nome, boolean isPreferencial) {
 		this.codigo = codigo;
--- src/br/com/fnr_tap/entidades/Produto.java
+++ src/br/com/fnr_tap/entidades/Produto.java
@@ -4,16 +4,34 @@ public class Produto {
 	private int codigo;
 	private String descricao;
 	private double preco;
-	
-	public int getCodigo() {	return codigo;	}
-	public void setCodigo(int codigo) {	this.codigo = codigo;	}
-	public String getDescricao() {	return descricao;	}
-	public void setDescricao(String descricao) {	this.descricao = descricao;}
-	public double getPreco() {	return preco;	}
-	public void setPreco(double preco) {	this.preco = preco;	}
 
-	public Produto() {	}
-	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+	public Produto() {
+	}
+
 	public Produto(int codigo, String descricao, double preco) {
 		this.codigo = codigo;
 		this.descricao = descricao;
